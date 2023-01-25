f46557dc137ae21be0009b4ab5cb7fbe01e80348
DZUY Nguyen Duc <terragodof@gmail.com>
2023-01-25 17:36:11 +0700

    Update SETUP.bat

--- SETUP.bat
+++ SETUP.bat
@@ -5,11 +5,11 @@ echo .
 echo .
 echo .
 echo ------------------------------------------------------
-echo - HEL OS v1.5test/11142022			   -
-echo - (c) 1994 High Entropy Labs.				   -
-echo - Everett, Maine, USA.					   -
-echo - A recovered copy of HEL OS v1.15 by Barnacl, 2022. -  
-echo - Based on FreeDOS with a li'l UNIX inspiration.	   -
+echo - HEL OS v1.5rtest/01182023			   
+echo - (c) 1994 High Entropy Labs.				   
+echo - Everett, Maine, USA.					   
+echo - A recovered copy of HEL OS v1.15 by Barnacl, 2022-2023.   
+echo - Based on FreeDOS with a li'l UNIX inspiration.	   
 echo ------------------------------------------------------
 echo ........READY.
 
