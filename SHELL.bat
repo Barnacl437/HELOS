cf663bdec06a37dcb95223f400a66e5d2f43ef26
DZUY Nguyen Duc <terragodof@gmail.com>
2023-07-12 11:43:53 +0700

    adding aliases, moding versions

--- SHELL.bat
+++ SHELL.bat
@@ -7,11 +7,12 @@ echo .
 echo .
 echo .
 echo ------------------------------------------------------
-echo - HEL OS v1.5rtest_unstb/20230125			   
+echo - HEL OS v1.5rtest_unstb/20230711 (shitty)			   
 echo - (c) 1994 High Entropy Labs.				   
 echo - Everett, Maine, USA.					   
 echo - A recovered copy of HEL OS v1.15 by Barnacl, 2022-2023.   
 echo - Based on FreeDOS with a li'l UNIX inspiration.	   
 echo ------------------------------------------------------
 echo ........READY.
+prompt shitty// 
 
